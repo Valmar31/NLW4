3d270b3b29993310311c4bfc984a7d9680c155e0
unknown <valmar_mmm@hotmail.com>
2021-02-25 09:23:36 -0300

    contador

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,17 +1,28 @@
+import { useState } from 'react';
+
 import styles from '../styles/components/Countdown.module.css';
 
 export function Countdown() {
+    const [time, setTime] = useState(25 * 60);
+
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+
+    // preencher começo com 0 (padstart). Dividir a string em 2 (split). ****  25= '2' '5'**** '5' = '0' '5'
+    const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+    const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+
     return(
     <div>
         <div className={styles.countdownContainer}>
             <div>
-                <span>2</span>
-                <span>5</span>
+                <span>{minuteLeft}</span>
+                <span>{minuteRight}</span>
             </div>
             <span>:</span>
-            <div>
-                <span>0</span>
-                <span>0</span>
+            <div> 
+                <span>{secondLeft}</span>
+                <span>{secondRight}</span>
             </div>
         </div>
 
