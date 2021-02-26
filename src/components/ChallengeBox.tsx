b4d17aa0dffd56368e804a04a1aa1036cd0979ff
unknown <valmar_mmm@hotmail.com>
2021-02-26 10:50:59 -0300

    3124

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -6,7 +6,30 @@ export function ChallengeBox() {
     return (
         <div className={styles.challengeBoxContainer}>
             { hasActiveChallenge ? (
-                <div />
+                <div className={styles.challengeActive}>
+                    <header>Ganhe 400 xp</header>
+
+                    <main>
+                        <img src="icons/body.svg" alt=""/>
+                        <strong>Novo desafio</strong>
+                        <p>Levante e faça uma caminhada de 3 minutos.</p>
+                    </main>
+
+                    <footer>
+                        <button
+                         type="button"
+                         className={styles.challengeFailedButton}
+                        >
+                            Falhei
+                        </button>
+                        <button
+                         type="button"
+                         className={styles.challengeSucceededButton}
+                        >
+                            Completei
+                        </button>
+                    </footer>
+                </div>
             ) : (
                 <div className={styles.challengeNotActive}>
                 <strong>Finalize um ciclo para receber um desafio</strong>
