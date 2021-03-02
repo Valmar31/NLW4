3607174faffbecd34fdb7cce3091d5324cf676bb
unknown <valmar_mmm@hotmail.com>
2021-03-02 13:55:52 -0300

    Aula 4 completa

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,48 +1,21 @@
-import { useState, useEffect, useContext } from 'react';
-import { ChallengesContext } from '../contexts/ChallengesContext';
+import { useContext } from 'react';
+import { CountdownContext } from '../contexts/CountdownContext';
+
 import styles from '../styles/components/Countdown.module.css';
 
-let countdownTimeout: NodeJS.Timeout;
-
 export function Countdown() {
-    const { startNewChallenge } = useContext(ChallengesContext);
-
-
-    const [time, setTime] = useState(0.1 * 60);
-    const [isActive, setIsActive] = useState(false);
-    const [hasFinished, setHasFinished] = useState(false);
-
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
-
+    const { 
+        minutes, 
+        seconds, 
+        hasFinished, 
+        isActive, 
+        startCountdown, 
+        resetCountdown
+    } = useContext(CountdownContext)
+    
     // preencher começo com 0 (padstart). Dividir a string em 2 (split). ****  25= '2' '5'**** '5' = '0' '5'
     const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
     const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
-
-    function startCountdown() {
-        setIsActive(true);
-    }
-
-    function resetCountdown() {
-        clearTimeout(countdownTimeout);
-        setIsActive(false);
-        // resetar o timer
-        setTime (0.1 * 60);
-    }
-
-    useEffect(() => {
-        // console.log(isActive);
-        if (isActive && time > 0) {
-            countdownTimeout = setTimeout(() => {
-                setTime(time - 1); 
-            }, 1000);
-        } else if (isActive && time == 0) {
-            // console.log('finalizou');
-            setHasFinished(true);
-            setIsActive(false);
-            startNewChallenge();  
-        }
-    }, [isActive, time])
 
     return(
     <div>
