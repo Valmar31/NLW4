b9f9a1f74c4528c997924fa5cb4924bcb85a0c88
unknown <valmar_mmm@hotmail.com>
2021-04-05 09:12:19 -0300

    cookies terminados

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,49 +8,61 @@ import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 
+//import React from 'react';
 import styles from '../styles/pages/Home.module.css';
 import { CountdownProvider } from "../contexts/CountdownContext";
+import { ChallengesProvider } from '../contexts/ChallengesContext';
 
-export default function Home(props) {
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
+
+export default function Home(props: HomeProps) {
   // console.log(props);
 
   return (
-  <div className={styles.container}>
-    <Head>
-      <title>Início | move.it</title>
-    </Head>
+    <ChallengesProvider 
+      level={props.level} 
+      currentExperience={props.currentExperience}
+      challenges={props.challengesCompleted}
+    >
+      <div className={styles.container}>
+        <Head>
+          <title>Início | move.it</title>
+        </Head>
 
-    <ExperienceBar />
+        <ExperienceBar />
 
-    <CountdownProvider>
-      <section>
-        <div>
-          <Profile />
-          <CompletedChallenges />
-          <Countdown />
-        </div>
-        <div>
-          <ChallengeBox />
-        </div>
-      </section>
-    </CountdownProvider>
+        <CountdownProvider>
+          <section>
+            <div>
+              <Profile />
+              <CompletedChallenges />
+              <Countdown />
+            </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
 
-  </div>
+      </div>
+
+    </ChallengesProvider>
   )
 }
 
 //cookies
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  //chamada api
-  const user = {
-    level: 1,
-    currentExperience: 50,
-    challengesCompleted: 2,
-  }
-
-  console.log('user');
+export const getServerSideProps: GetServerSideProps = async (ctx) => { 
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
 
   return {
-     props: user 
+     props: {
+       level: Number(level), //ou level: +level,
+       currentExperience: Number(currentExperience),
+       challengesCompleted: Number(challengesCompleted),
+     }
   }
 }
